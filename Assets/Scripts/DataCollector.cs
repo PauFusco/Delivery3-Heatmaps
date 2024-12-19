d62c102fabb04f4f40c0a97d0fc2b46f9a61dd6d
VicMarBall <99729766+VicMarBall@users.noreply.github.com>
2024-12-19 14:59:25 +0100

    data collector script

---
+++ Assets/Scripts/DataCollector.cs
@@ -0,0 +1,36 @@
+using Gamekit3D;
+using Gamekit3D.Message;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataCollector : MonoBehaviour, IMessageReceiver
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void OnReceiveMessage(MessageType type, object sender, object msg)
+	{
+		switch (type)
+		{
+			case MessageType.DEAD:
+				//Death((Damageable.DamageMessage)msg);
+				break;
+			case MessageType.DAMAGED:
+				//ApplyDamage((Damageable.DamageMessage)msg);
+				break;
+			default:
+				break;
+		}
+	}
+
+}
