d2128a1e33a40388f4c9af0cab1f63b12a026c1c
PauFusco <99730051+PauFusco@users.noreply.github.com>
2024-12-20 13:48:15 +0100

    Update PHPConnection.cs
    baseline function for connection to the php server

--- Assets/Scripts/PHPConnection.cs
+++ Assets/Scripts/PHPConnection.cs
@@ -1,10 +1,29 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class PHPConnection : MonoBehaviour
 {
     private void OnEnable()
     {
+    }
+
+    private IEnumerator PHPWebRequest(WWWForm form, string link, Action<uint> callback)
+    {
+        UnityWebRequest webRequest = UnityWebRequest.Post(link, form);
+
+        yield return webRequest.SendWebRequest();
+
+        if (webRequest.result != UnityWebRequest.Result.Success)
+        {
+        }
+        else
+        {
+            uint callbackData = uint.Parse(webRequest.downloadHandler.text);
+
+            callback?.Invoke(callbackData);
+        }
     }
 }
