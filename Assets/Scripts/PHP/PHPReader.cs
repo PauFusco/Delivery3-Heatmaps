b32491ad1487bcbcc889487d34cbca88025bd7f0
PauFusco <99730051+PauFusco@users.noreply.github.com>
2025-01-07 18:02:48 +0100

    json file creates
    hits.json file is created when p is pressed
    json file contains all the information in the hits table from the sql server

--- Assets/Scripts/PHP/PHPReader.cs
+++ Assets/Scripts/PHP/PHPReader.cs
@@ -1,18 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class PHPReader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            ReadHits();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator PHPWebRequest(string link, string fileToWritePath)
     {
-        
+        UnityWebRequest webRequest = UnityWebRequest.Get(link);
+
+        yield return webRequest.SendWebRequest();
+
+        if (webRequest.result != UnityWebRequest.Result.Success)
+        {
+            // php request error handling
+            Debug.Log(webRequest.downloadHandler.text);
+        }
+        else
+        {
+            File.WriteAllText(fileToWritePath, webRequest.downloadHandler.text);
+        }
+    }
+
+    public void ReadHits()
+    {
+        string requestLink = "https://citmalumnes.upc.es/~victormb3/HITSREADER.php";
+
+        string filePath = Application.dataPath + "/JSONResponse/hits.json";
+        StartCoroutine(PHPWebRequest(requestLink, filePath));
     }
 }
