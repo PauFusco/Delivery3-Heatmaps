2689bcd9d1b43e88e3dd172c44035dc4d3e9f781
VicMarBall <99729766+VicMarBall@users.noreply.github.com>
2025-01-11 16:13:21 +0100

    heatmap scene + improving heatmaps window

---
+++ Assets/Heatmaps/HeatmapController.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeatmapController
+{
+	Material mMaterial;
+
+	float[] mPoints;
+	int mHitCount;
+
+	public void PrepareHeatmap(Material material)
+	{
+		mMaterial = material;
+
+		mPoints = new float[32 * 4]; // 32 point
+		mHitCount = 0;
+	}
+
+	public void AddHitPoint(float xp, float yp, float zp)
+	{
+		if (mHitCount >= 32)
+		{
+			Debug.Log("Cannot add another point in the heatmap (max 32)");
+			return;
+		}
+
+		mPoints[mHitCount * 4] = xp;
+		mPoints[mHitCount * 4 + 1] = yp;
+		mPoints[mHitCount * 4 + 2] = zp;
+		mPoints[mHitCount * 4 + 3] = Random.Range(1f, 3f);
+
+		mHitCount++;
+		mHitCount %= 32;
+
+		mMaterial.SetFloatArray("_Hits", mPoints);
+		mMaterial.SetInt("_HitCount", mHitCount);
+	}
+
+	public void Reset()
+	{
+		mPoints = new float[32 * 4]; // 32 point
+		mHitCount = 0;
+
+		mMaterial.SetFloatArray("_Hits", mPoints);
+		mMaterial.SetInt("_HitCount", mHitCount);
+	}
+}
